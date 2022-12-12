a332bafaf6d159078dcb2ab88fa247918ae8ad90
Pablo Gustavo <pablo1gustavo.dev@gmail.com>
2022-12-11 23:52:30 -0300

    ft: add TURN Action and includes in Game definition.

--- src/definitions/Game.h
+++ src/definitions/Game.h
@@ -1,12 +1,17 @@
 #ifndef GAME_DEF
 #define GAME_DEF
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
 #define MAX_LINE 100
 #define MAX_ACTION 10
 #define MAX_ID_SIZE 10
 
 typedef enum GameAction {
-    SAY, DISCARD, BUY
+    SAY, DISCARD, BUY, TURN
 } Action;
 
 #endif
