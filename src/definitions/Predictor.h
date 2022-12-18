11b1c2b453893a4c93c324b91c0f8b95a594ebd3
Pablo Gustavo <pablo1gustavo.dev@gmail.com>
2022-12-18 20:01:39 -0300

    ft: add Predictor definition with Analyzer struct.

---
+++ src/definitions/Predictor.h
@@ -0,0 +1,14 @@
+#ifndef PREDICTOR_DEF
+#define GAME_DEF
+
+#include "Card.h"
+
+// map to count values and suits
+typedef struct
+{
+    int values[VALUES_NUM];
+    int suits[SUITS_NUM];
+}
+Analyzer;
+
+#endif
