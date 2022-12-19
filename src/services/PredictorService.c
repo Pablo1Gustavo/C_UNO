b9f8aac8bcfbcda27ce31a45b825984ed411b149
Pablo Gustavo <pablo1gustavo.dev@gmail.com>
2022-12-18 23:23:51 -0300

    ft: add base choose and calc function in PredictorService.

--- src/services/PredictorService.c
+++ src/services/PredictorService.c
@@ -3,12 +3,13 @@
 
 #include <string.h>
 
+#include "CardService.c"
 #include "../definitions/Predictor.h"
 
 void initAnalyzer(Analyzer* analyzer, Hand hand, int handSize)
 {
-    memset(analyzer->values, 0, VALUES_NUM * sizeof(int));
-    memset(analyzer->suits, 0, SUITS_NUM * sizeof(int));
+    memset(analyzer->values, 0, VALUES_NUM * sizeof(Value));
+    memset(analyzer->suits, 0, SUITS_NUM * sizeof(Suit));
 
     for (int i = 0; i < handSize; i++)
     {
@@ -27,6 +28,60 @@ void revertAnalyzer(Analyzer* analyzer, Card card)
 {
     analyzer->values[card.value]--;
     analyzer->suits[card.suit]--;
+}
+
+Suit chooseSuit(Analyzer self)
+{
+    Suit desired = DIAMONDS;
+
+    for (Suit s = 0; s < SUITS_NUM; s++)
+    {
+        if (self.suits[s] > self.suits[desired])
+        {
+            desired = s;
+        }
+    }
+
+    return desired;
+}
+
+float calcStatus(Analyzer self, Analyzer oppoent, Card card, bool isValue)
+{
+    if (isValue)
+    {
+        float valueStatus = self.values[card.value] * SELF_WEIGHT;
+        valueStatus -= oppoent.values[card.value] * OPPONENT_WEIGHT;
+
+        return valueStatus;
+    }
+
+    float suitStatus = self.suits[card.suit] * SELF_WEIGHT;
+    suitStatus -= oppoent.suits[card.suit] * OPPONENT_WEIGHT;
+
+    return suitStatus;
+}
+
+int chooseCard(Analyzer self, Analyzer opponent, Card table, Hand hand, int handSize)
+{
+    float max = 0;
+    int maxIndex = -1;
+
+    for (int i = 0; i < handSize; i++)
+    {
+        if (canPutTable(hand[i], table))
+        {
+            float currValueStatus   = calcStatus(self, opponent, hand[i], 1);
+            float currSuitStatus    = calcStatus(self, opponent, hand[i], 0);
+
+            if (currValueStatus + currSuitStatus > max)
+            {
+                max = currValueStatus + currSuitStatus;
+                maxIndex = i;
+            }
+        }
+    }
+
+    return maxIndex;
 }
 
 #endif
